76212322c2c9dbd9ee00d148bf091e7a2c97e2ee
Kristian <kristianuruplarsen@gmail.com>
2019-08-20 20:24:49 +0200

    small changes

--- fresh.sh
+++ fresh.sh
@@ -1,53 +1,46 @@
 #!/bin/bash/
+apt-get update
 
-sudo apt-get update
+n_steps = 6
+
+echo "( 1/$n_steps ) Installing vscode from snap"
+snap install code-insiders
 
 
-echo "Installing python 3.6 through miniconda"
+echo " --> Installing VScode extentions"
+code-insiders --install-extention James-Yu.latex-workshop
+code-insiders --install-extention karyfoundation.theme-karyfoundation-themes
+code-insiders --install-extention ms-python.python
+code-insiders --install-extention ms-vscode-remote.remote-ssh
+code-insiders --install-extention ms-vscode-remote.remote-ssh-edit
+code-insiders --install-extention ms-vscode-remote.remote-ssh-explorer
 
+
+echo "( 2/$n_steps ) Installing evolution mail client"
+apt install evolution
+apt install evolution-ews
+
+
+echo "( 3/$n_steps ) Installing python through miniconda"
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p ~/miniconda
 rm ~/miniconda.sh
 
-echo "    Installing core python packages"
 
-pip install numpy
-pip install pandas
-pip install geopandas
-pip install scikit-learn
-pip install networkx
-pip install matplotlib
-pip install seaborn
-
-
-echo "Installing TeX-Live"
-
+echo "( 4/$n_steps )Installing TeX-Live"
 yes y | sudo apt-get install texlive-full
 
-echo "Installing Atom (stable)"
 
-yes y | sudo apt install curl
-
-curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+echo "( 5/$n_steps ) Setting up zsh"
+apt install zsh
+chsh -s $(which zsh)
 
 
-sudo apt-get update
-yes | sudo apt-get install atom
+echo " --> getting oh-my-zsh"
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 
 
-echo "    Installing core atom packages"
-
-apm install hydrogen
-apm install atom-latex
-apm install pdf-view
-apm install file-icons
-
-echo "Installing GitKraken"
-
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-dpkg -i gitkraken-amd64.deb
-
-yes y | apt-get install libgnome-keyring-common libgnome-keyring-dev
-
+echo "( 6/$n_steps ) Installing vimrc"
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_basic_vimrc.sh
 
